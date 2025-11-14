9e5178cf490e2159de9ded1d9b720ede6ae1d585
Sebastian <sebas.programador@outlook.com>
2025-11-13 19:52:05 -0500

    feat: :fire: Remover archivos y código innecesario

---
+++ src/views/PokemonDetailView.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="text-xl">detalle pokemon</div>
+</template>
+<script setup lang="ts"></script>
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import { usePokemonStore } from '@/stores/pokedex'
+import PokemonItem from '@/components/PokemonItem.vue'
+
+const pokemonStore = usePokemonStore()
+
+onMounted(() => {
+  pokemonStore.getPokemon()
+})
+</script>
+<template>
+  <div class="text-3xl font-bold underline">listado de pokemon</div>
+  <PokemonItem />
+</template>
